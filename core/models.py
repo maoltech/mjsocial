cd0026edea52c3e95f1bfa6a330ef807cdaa6272
unknown <hardeyshinnah.mo@gmail.com>
2022-12-20 07:14:48 -0800

    20th sign signup

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 # Create your models here.
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_user = models.IntegerField
+    id_user = models.IntegerField()
     bio = models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
     location=models.CharField(max_length=100, blank=True)
--- core/views.py
+++ core/views.py
@@ -29,11 +29,30 @@ def signup(request):
 
 
                 user_model = User.objects.get(username=username)
-                new_profile =Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile = Profile.objects.create(user=user_model, id_userr=user_model.id)
                 new_profile.save()
                 return redirect('signup')
         else:
             messages.info(request, 'Password Does Not Match')
             return redirect('signup')
     else:
-        return render(request, 'signup.html') 
+        return render(request, 'signup.html')
+
+
+def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+
+    else:
+        return render(request, 'signin.html')
