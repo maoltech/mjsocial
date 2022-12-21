64c8e1889eae38361222ed86e4b5786a3d0af439
unknown <hardeyshinnah.mo@gmail.com>
2022-12-21 09:13:38 -0800

    21th commit

--- core/models.py
+++ core/models.py
@@ -6,10 +6,12 @@ User = get_user_model()
 # Create your models here.
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_user = models.IntegerField()
+    id_user = models.IntegerField(unique=True, null=True)
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
-    location=models.CharField(max_length=100, blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='avatar.png')
+    location = models.CharField(max_length=100, blank=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
 
 
     def __str__(self):
--- core/views.py
+++ core/views.py
@@ -13,7 +13,40 @@ def index(request):
 
 @login_required(login_url='signin')
 def settings(request):
-    return render(request, 'setting.html')
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.first_name = first_name
+            user_profile.last_name = last_name
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('images')
+            bio = request.POST['bio']
+            location = request.POST['location']
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.first_name = first_name
+            user_profile.last_name = last_name
+            user_profile.save()      
+
+        return redirect('settings')    
+
+    return render(request, 'setting.html', {'user_profile': user_profile})
 
 def signup(request):
 
@@ -38,7 +71,7 @@ def signup(request):
                 auth.login(request, user_login)
 
                 user_model = User.objects.get(username=username)
-                new_profile = Profile.objects.create(user=user_model, id_userr=user_model.id)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
                 return redirect('settings')
         else:
